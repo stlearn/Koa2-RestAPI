953cbbc404b49c466f7f68c7a236d96005ef2b44
gao <yunfgao@qq.com>
2021-03-28 18:11:24 +0800

    Merge branch 'main' into 可复用组件

--- src/routes/main-routes.js
+++ src/routes/main-routes.js
@@ -5,11 +5,6 @@ const router = new KoaRouter()
 
 export default router
   .post('/upload', controllers.upload)
-<<<<<<< HEAD
-=======
-  //测试
-  .all('/public/test',controllers.test)
->>>>>>> main
   .post('/login',controllers.login)
   .get('/publish/addgoods',controllers.goods)
   .get('/test',controllers.testController)
